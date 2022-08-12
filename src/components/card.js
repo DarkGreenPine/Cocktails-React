4469c3c0d682c3fd4bc4034b044c83ba7ce3c526
DarkGreenPine <ismoore@protonmail.com>
2022-08-12 14:29:43 -0400

    initial file setup

---
+++ src/components/card.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Card = ({drink,loading,drinkInfo}) => {
+    return (
+        <>
+            loading ? <h1>Loading...</h1>
+
+        </>
+    )
+}
+
+export default Card;
---
+++ src/components/drink-info.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const DrinkInfo = ({data}) => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default DrinkInfo;
